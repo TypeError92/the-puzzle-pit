470a58363de730c711e13757c9785e6db11e64c7
TypeError92 <simonwalgenbach@posteo.de>
2023-11-03 16:56:43 +0000

    creates connections between clue_tiles and neighbours on Mosaic.__init__

--- tests/mosaic_test.py
+++ tests/mosaic_test.py
@@ -1,4 +1,4 @@
-from the_puzzle_pit.mosaic.mosaic import Mosaic
+from the_puzzle_pit.mosaic.mosaic import Mosaic, _Tile
 
 easy_5x5 = (
     [
@@ -26,6 +26,72 @@ class TestMosaicInit:
         except:
             assert False
 
+    def test_mosaic_init_01(self):
+        """Should create:
+        - a .grid property with a matrix matching the shape of the input and containing:
+            - a cell with a clue for each input value >= 0
+            - a cell with clue is None for each input value == -1
+        - a .clue_cells property listing only those cells containing a clue"""
+        clues = easy_5x5[0]
+        mosaic = Mosaic(clues)
+        assert len(mosaic) == 5
+        assert all([len(row) == 5 for row in mosaic])
+        assert len(mosaic.clue_tiles) == 9
+
+    def test_mosaic_init_02(self):
+        """Allows for retrieval of original input via .clues property."""
+        clues = easy_5x5[0]
+        mosaic = Mosaic(clues)
+        assert mosaic.clues == clues
+
+    def test_mosaic_init_03(self):
+        """Accepts input values of -2 as 'fillers' for non-rectangular shapes and marks these as None in the .grid."""
+        clues = [
+            [-1, 2, -1, -1, -2],
+            [-1, -1, 2, -1, -1],
+            [6, -1, 3, -1, 3],
+            [-1, -1, 3, -1, 4],
+            [-1, 3, -1, -1, -1]
+        ]
+        mosaic = Mosaic(clues)
+        assert mosaic[0][4] is None
+
+    def test_mosaic_init_04(self):
+        """Creates an "in" connection between each tile and all of its neighbouring clue_tiles (including itself where appropriate)."""
+        clues = easy_5x5[0]
+        mosaic = Mosaic(clues)
+        assert [[len(tile.connections_in) for tile in row] for row in mosaic] == [
+            [1, 2, 2, 2, 1],
+            [2, 4, 3, 4, 2],
+            [1, 4, 3, 5, 2],
+            [2, 4, 3, 4, 2],
+            [1, 2, 2, 2, 1]
+        ]
+
+    def test_mosaic_init_05(self):
+        """Creates an "out" connection between each clu_tile and all of its neighbouring tiles (including itself)."""
+        clues = easy_5x5[0]
+        mosaic = Mosaic(clues)
+        assert [len(clue_tile.connections_out) for clue_tile in mosaic.clue_tiles] == [6, 4, 9, 6, 9, 6, 9, 6, 6]
+
+
+class TestMosaicUtils:
+    def test_mosaic_flatten(self):
+        """Mosaic.flat() returns a flattened list containing all the mosaic's tiles."""
+        clues = easy_5x5[0]
+        mosaic = Mosaic(clues)
+        flattened = mosaic.flatten()
+        assert len(flattened) == 25
+        assert all([isinstance(tile, _Tile) for tile in flattened])
+
+
+class TestMosaicInputValidation:
+    pass
+
 
 class TestMosaicFunctionality:
+    pass
+
+
+class TestTile:
     pass
--- the_puzzle_pit/mosaic/mosaic.py
+++ the_puzzle_pit/mosaic/mosaic.py
@@ -1,3 +1,55 @@
-class Mosaic:
-    def __init__(self, clues):
-        pass
+class _Tile:
+    def __init__(self, clue):
+        if clue == -1:
+            self._clue = None
+        else:
+            self._clue = clue
+            self.connections_out = []
+        self.connections_in = []
+
+    @property
+    def clue(self):
+        return self._clue
+
+class Mosaic(list):
+    def __init__(self, clues: list[list[int, ...], ...]):
+        super().__init__()
+        self._clues = clues
+        self._clue_tiles = []
+        self._height = len(clues)
+        self._width = len(clues[0])
+        for clue_row in clues:
+            row = []
+            for clue in clue_row:
+                if clue == -2:
+                    row.append(None)
+                elif -1 <= clue <= 9:
+                    tile = _Tile(clue)
+                    row.append(tile)
+                    if clue >= 0:
+                        self._clue_tiles.append(tile)
+            self.append(row)
+
+        # create connections
+        for (i, row) in enumerate(self):
+            for (j, tile) in enumerate(row):
+                if tile.clue is not None:
+                    for (di, dj) in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)):
+                        other_i = i + di
+                        other_j = j + dj
+                        if 0 <= other_i < self._height and 0 <= other_j < self._width:
+                            other_tile = self[other_i][other_j]
+                            tile.connections_out.append(other_tile)
+                            other_tile.connections_in.append(tile)
+
+
+    @property
+    def clues(self):
+        return [[tile.clue if tile is not None else None for tile in row] for row in self]
+
+    @property
+    def clue_tiles(self):
+        return self._clue_tiles
+
+    def flatten(self):
+        return [tile for row in self for tile in row]
