0442f32a09a1f78a583cc67e2a71be2b6ac79872
TypeError92 <simonwalgenbach@posteo.de>
2023-11-03 15:22:22 +0000

    add basic __init__ for Mosaic class

--- tests/mosaic_test.py
+++ tests/mosaic_test.py
@@ -1,1 +1,31 @@
 from the_puzzle_pit.mosaic.mosaic import Mosaic
+
+easy_5x5 = (
+    [
+        [-1, 2, -1, -1, 1],
+        [-1, -1, 2, -1, -1],
+        [6, -1, 3, -1, 3],
+        [-1, -1, 3, -1, 4],
+        [-1, 3, -1, -1, -1]
+    ],
+    [
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+        [1, 0, 0, 1, 1]
+    ]
+)
+
+
+class TestMosaicInit:
+    def test_mosaic_init_00(self):
+        """Should accept an n x n matrix of integers [-1, 9] as its only argument."""
+        try:
+            Mosaic(easy_5x5[0])
+        except:
+            assert False
+
+
+class TestMosaicFunctionality:
+    pass
--- the_puzzle_pit/mosaic/mosaic.py
+++ the_puzzle_pit/mosaic/mosaic.py
@@ -1,2 +1,3 @@
 class Mosaic:
-    pass
+    def __init__(self, clues):
+        pass
